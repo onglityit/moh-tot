1beebd7c4854a47e5ecab4263f710e7016ce6ea7
Darren Ong <onglityit@entomo.co>
2024-02-23 09:17:56 +0800

    Day1 Java 01 - basic workable springboot

---
+++ TOT-integration/JAVA/day1-java01/day1-java01/src/main/java/org/mysj/Main.java
@@ -0,0 +1,13 @@
+package org.mysj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "org.mysj")
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
