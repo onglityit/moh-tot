87f57e1dbb7f4a038afe20eb3347a21cebdbd84e
Darren Ong <onglityit@entomo.co>
2024-02-23 17:35:13 +0800

    php use properties

--- TOT-integration/PHP/hellojava-solution/index.php
+++ TOT-integration/PHP/hellojava-solution/index.php
@@ -7,7 +7,7 @@ header("Access-Control-Allow-Methods: GET");
 
 function properties(){
     return [
-        'java01' => 'http://localhost:8081/day1-java01/helloworld',
+        // 'java01' => 'http://localhost:8081/day1-java01/helloworld',
         'java02' => 'http://localhost:8082/day1-java02/helloworld' 
     ];
 }
@@ -16,7 +16,7 @@ function main(){
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $curl = curl_init();
         curl_setopt_array($curl, array(
-        CURLOPT_URL => 'http://localhost:8082/day1-java02/helloworld',
+        CURLOPT_URL => properties()['java02'],
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
