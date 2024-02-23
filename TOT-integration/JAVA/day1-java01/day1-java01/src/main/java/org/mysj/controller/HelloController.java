4a0aa6a1c7259bdce79e2339856888639e03575c
Darren Ong <onglityit@entomo.co>
2024-02-23 15:32:35 +0800

    commented out solution

--- TOT-integration/JAVA/day1-java01/day1-java01/src/main/java/org/mysj/controller/HelloController.java
+++ TOT-integration/JAVA/day1-java01/day1-java01/src/main/java/org/mysj/controller/HelloController.java
@@ -9,4 +9,8 @@ public class HelloController {
     public String hello() {
         return "hello from Java 01";
     }
+    @GetMapping("/helloworld2")
+    public String hello2() {
+        return "ready to implement...";
+    }
 }
---
+++ TOT-integration/JAVA/day1-java01/day1-java01/src/main/java/org/mysj/controller/HelloControllerSolution.java
@@ -0,0 +1,35 @@
+package org.mysj.controller;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class HelloControllerSolution {
+//    @GetMapping("/helloworld-solution")
+//    public String hello() {
+//        return "hello from Java 01";
+//    }
+//    @GetMapping("/helloworld2-solution")
+//    public String hello2() throws IOException {
+//        OkHttpClient client = new OkHttpClient().newBuilder()
+//                .build();
+//        Request request = new Request.Builder()
+//                .url("http://localhost:8082/day1-java02/helloworld")
+//                .method("GET", null)
+//                .build();
+//        try (Response response = client.newCall(request).execute()) {
+//            if (response.isSuccessful()) {
+//                return response.body().string();
+//            } else {
+//                return "Error: " + response.code();
+//            }
+//        }
+//    }
+}
