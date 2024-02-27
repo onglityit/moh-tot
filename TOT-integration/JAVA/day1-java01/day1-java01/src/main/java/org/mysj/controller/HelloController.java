6ef411ccb6a26923a709c28877b9b4b4e6154c36
Darren Ong <onglityit@entomo.co>
2024-02-27 09:55:23 +0800

    nodejs challenge question

--- TOT-integration/JAVA/day1-java01/day1-java01/src/main/java/org/mysj/controller/HelloController.java
+++ TOT-integration/JAVA/day1-java01/day1-java01/src/main/java/org/mysj/controller/HelloController.java
@@ -3,15 +3,33 @@ package org.mysj.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 public class HelloController {
     @GetMapping("/helloworld")
     public String hello() {
         return "hello from Java 01";
     }
-    @GetMapping("/helloworld2")
-    public String hello2() {
-        return "ready to implement...";
+//    @GetMapping("/helloworld2")
+//    public String hello2() {
+//        return "ready to implement...";
+//    }
+    @GetMapping("/helloworld2-solution")
+    public String hello2() throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("http://localhost:8082/day1-java02/helloworld")
+                .method("GET", null)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                return response.body().string();
+            } else {
+                return "Error: " + response.code();
+            }
+        }
     }
     @GetMapping("/hellonode")
     public String hellonode() {return "ready to be lingua franca of programming...";}
--- TOT-integration/NODEJS/nodejs01/node01.js
+++ TOT-integration/NODEJS/nodejs01/node01.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const multer = require('multer');
 
+const upload = multer({ dest: 'uploads/' });
 app.get('/helloworld', (req, res) => {
     res.send('Hello World from NodeJS 01');
+});
+app.post('/central-vaccine', upload.single('csv'), (req, res) => {
+  const uploadedFile = req.file;
+  res.send('File uploaded successfully');
 });
 
 app.listen(port, () => {
